5da969b76cee2ad64382d25fbad66886f764c126
Bruno Hubner <brunohubnerdev@gmail.com>
2022-04-30 13:23:28 -0300

    style: lint format

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common"
 import { MongooseModule } from "@nestjs/mongoose"
 import { env } from "./config/env"
 import { PlayersModule } from "./players/players.module"
-import { CategoriesModule } from './categories/categories.module';
-import { ChallengesModule } from './challenges/challenges.module';
+import { CategoriesModule } from "./categories/categories.module"
+import { ChallengesModule } from "./challenges/challenges.module"
 
 @Module({
     imports: [
